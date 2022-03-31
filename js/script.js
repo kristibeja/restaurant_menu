90ee161974e3716e3dc6a2d1b3ec87bb6041fb00
unknown <kristibeja3@gmail.com>
2022-03-31 21:48:53 +0200

    function added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// search bar
+function search_menu() {
+    let inputValue = document.getElementById('searchbar').value;
+    inputValue = inputValue.toLowerCase();
+    let itemName = document.getElementsByClassName('menu-item-name');
+    let menuItem = document.getElementsByClassName('menu-item');
+    let itemDescription = document.getElementsByClassName('menu-item-description');
+      
+    for (i = 0; i < menuItem.length; i++) { 
+        if (!itemName[i].innerHTML.toLowerCase().includes(inputValue) && !itemDescription[i].innerHTML.toLowerCase().includes(inputValue)) {
+            menuItem[i].style.display="none";
+        }
+        else {
+            menuItem[i].style.display="menu-item";                 
+        }
+    }
+}
